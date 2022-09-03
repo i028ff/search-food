9aba2d94e9f1fc7aaef18f95d795e4bc08586463
i028ff <kousuke111125@gmail.com>
2022-09-03 14:33:55 +0900

    version1

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def search_index():
         # print(geo_data['latitude'])
         # print(geo_data['longitude'])
         loc = {'lat': 33.9578385, 'lng': 131.281945} # 軽度・緯度を取り出す
+        #loc = {'lat': latitude, 'lng': longitude}
         place_results = client.places_nearby(location=loc, radius=10000, keyword={word} ,language='ja') #半径1000m以内のカフェ情報を取得
         #pprint.pprint(place_results)
         results = []
@@ -48,7 +49,7 @@ def search_index():
                 photos.append(photo)
         return render_template('search.html',results=results,photos=photos,p_values=p_values)
     else:
-        return render_template('main.html')
+        return render_template('search.html')
 
 @app.route('/detail/<string:id>/<p_ref>')
 def detail(id,p_ref):
